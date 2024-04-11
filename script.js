4ebd24f3f73a831141233f7693316f8ea50be260
Amit_Valand <143027284+Amitvaland14@users.noreply.github.com>
2024-04-11 15:26:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const slider = document.querySelector('.slider');
-
+const next = document.querySelector(".next")
 function activate(e) {
   const items = document.querySelectorAll('.item');
   e.target.matches('.next') && slider.append(items[0]);
   e.target.matches('.prev') && slider.append(items[items.ngth -1]);
 }
 document.addEventListener('click',activate,false);
+
+setInterval(() => {
+    next.click();
+}, 1800);
